6fcbd43adaaa1ddb78e05b7c6e53774cc895128f
hades200721 <glebp@post.bgu.ac.il>
2017-07-01 04:47:24 +0300

    redirecting function + ui for playlist step 2

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -24,27 +24,6 @@ export class PlaylistComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    // this.playlist = [{
-    //   artist: 'Mike Posner',
-    //   description: 'Mike Posner - Please Dont Go (Adventure Club D...',
-    //   duration: 256253,
-    //   durationStr: '04:16',
-    //   genre: 'Dubstep Electronic',
-    //   id: 8127112,
-    //   imagePath: 'https://i1.sndcdn.com/artworks-000003643515-yzt2o1-t500x500.jpg',
-    //   name: 'Mike Posner - Please Dont Go (Adventure Club D...',
-    // },
-    // {
-    //   artist: 'Wayne Wonder ',
-    //   description: '',
-    //   duration: 128570,
-    //   durationStr: '02:08',
-    //   genre: 'reggae',
-    //   id: 32995173,
-    //   imagePath: 'https://i1.sndcdn.com/artworks-000125406210-n1qj4g-t500x500.jpg',
-    //   name: 'Wayne Wonder - No Letting Go'
-    // }
-    // ];
     this.dataStorageService.getPlaylistSongs();
     this.subscription = this.playlistService.playlistChanged
       .subscribe(
@@ -53,12 +32,11 @@ export class PlaylistComponent implements OnInit {
       }
       )
 
-    // this.dataStorageService.getPlaylistSongs().subscribe(
-    //   (songs: any) => {
-    //     debugger;
-    //     this.playlist = songs;
-    //   }
-    // )
+  }
+
+  removeFromPlaylist(index: number) {
+    this.playlistService.removeSongFromPlaylist(index);
+    this.dataStorageService.savePlaylist();
   }
 
   ngOnDestroy() {
